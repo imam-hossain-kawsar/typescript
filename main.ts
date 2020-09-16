791d46113da931c2220f8bb503106e7e4adc45f5
Imam Hossain Kawsar <kawsaritdu2015@gmail.com>
2020-09-17 02:08:52 +0600

    added interface

--- main.ts
+++ main.ts
@@ -236,3 +236,17 @@ this is a multi-line comment
 //    console.log(names[i]) 
 // }
 
+
+interface Person { 
+    age:number 
+ } 
+ 
+ interface Musician extends Person { 
+    instrument:string 
+ } 
+ 
+ var drummer = <Musician>{}; 
+ drummer.age = 27 
+ drummer.instrument = "Drums" 
+ console.log("Age:  "+drummer.age) 
+ console.log("Instrument:  "+drummer.instrument)
