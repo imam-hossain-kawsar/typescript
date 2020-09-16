b3dfb81087f6858752e125daae54d451ec9e59da
Imam Hossain Kawsar <kawsaritdu2015@gmail.com>
2020-09-17 00:15:15 +0600

    added some variable of different type

--- main.js
+++ main.js
@@ -4,13 +4,18 @@ this is a multi-line comment
 */
 // var message:string = "Hello world"
 // console.log(message);
-var Gretting = /** @class */ (function () {
-    function Gretting() {
-    }
-    Gretting.prototype.greet = function () {
-        console.log("Hello world!!!");
-    };
-    return Gretting;
-}());
-var obj = new Gretting();
-obj.greet();
+// class Gretting{
+//     greet():void {
+//         console.log("Hello world!!!");
+//     }
+// }
+// var obj = new Gretting();
+// obj.greet();
+var Name = "kawsar";
+var score1 = 40;
+var score2 = 34;
+var sum = score1 + score2;
+console.log("name: " + Name);
+console.log("first score: " + score1);
+console.log("second score: " + score2);
+console.log("sum of the scores: " + sum);
--- main.ts
+++ main.ts
@@ -5,10 +5,24 @@ this is a multi-line comment
 // var message:string = "Hello world"
 // console.log(message);
 
-class Gretting{
-    greet():void {
-        console.log("Hello world!!!");
-    }
-}
-var obj = new Gretting();
-obj.greet();
+// class Gretting{
+//     greet():void {
+//         console.log("Hello world!!!");
+//     }
+// }
+// var obj = new Gretting();
+// obj.greet();
+
+
+var Name :string = "kawsar";
+var score1:number = 40;
+var score2:number = 34;
+var sum = score1+score2;
+console.log("name: "+Name);
+console.log("first score: "+score1);
+console.log("second score: "+score2);
+console.log("sum of the scores: "+sum);
+
+
+
+
