8a586607480d88f0eddf40fe40f60a4f4a8f0443
Imam Hossain Kawsar <kawsaritdu2015@gmail.com>
2020-09-17 00:08:07 +0600

    added class and create object

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-var message = "Hello world";
-console.log(message);
+//this is sing line comment
+/*
+this is a multi-line comment
+*/
+// var message:string = "Hello world"
+// console.log(message);
+var Gretting = /** @class */ (function () {
+    function Gretting() {
+    }
+    Gretting.prototype.greet = function () {
+        console.log("Hello world!!!");
+    };
+    return Gretting;
+}());
+var obj = new Gretting();
+obj.greet();
--- main.ts
+++ main.ts
@@ -2,5 +2,13 @@
 /*
 this is a multi-line comment
 */
-var message:string = "Hello world"
-console.log(message);
+// var message:string = "Hello world"
+// console.log(message);
+
+class Gretting{
+    greet():void {
+        console.log("Hello world!!!");
+    }
+}
+var obj = new Gretting();
+obj.greet();
